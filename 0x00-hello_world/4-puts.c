c8d0eaef6adea353d374638e5a0921d7f04faef9
Benaliyoussef1 <yb1979383@gmail.com>
2023-07-13 07:10:08 -0700

    o

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts functio
+* main - A program that prints a line with puts functio
 n
- * Return: 0 (Success)
- */
+* Return: 0 (Success)
+*/
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
