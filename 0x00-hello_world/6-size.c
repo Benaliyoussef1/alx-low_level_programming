99aa80a78f4d91e064f496e29a8beb44b120829a
Benaliyoussef1 <yb1979383@gmail.com>
2023-07-13 06:19:40 -0700

    lol

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - 6-size.c
- * description - Write a C program that 
+ * description - Write a C program that
  * prints the size of various types on
  * the computer it is compiled and run on.
  *
